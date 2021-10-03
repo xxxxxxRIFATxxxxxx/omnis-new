112ff3773aa4f0a4744b8ca5b47292fd5bc18dba
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-10-03 17:05:18 +0600

    Security page done

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+// For Switch Active
+let active = false;
+const image = document.querySelector(".switch-image");
+
+document.querySelector(".switch-active").addEventListener("click", () => {
+    active = !active;
+    if (active) {
+        image.src = "../images/switch-active.svg";
+    }
+
+    else {
+        image.src = "../images/switch.svg";
+    };
+});
